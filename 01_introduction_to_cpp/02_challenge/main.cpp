10a8e6d8b37066c7c5cfbee389c484b14e7a5b31
takayuki.asoma <takayuki.asoma@woven-planet.global>
2023-12-06 13:14:22 +0900

    01_02

---
+++ 01_introduction_to_cpp/02_challenge/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    // == や < などの関係演算子を使用して次の式が `true` になるようにしてください。
+    bool expr1 {6 < 7};
+    bool expr2 {10 > 9};
+    bool expr3{500 == 500};
+
+    // 論理演算子を使用して独自の式を作成してください。
+    // 各論理演算子を少なくとも1回は使用します。
+    bool expr4{true && false};
+    bool expr5{true || false};
+    bool expr6{!true};
+
+    // 次の行は変更しないでください。
+    // これらの行では、各ブール式がどのように評価されるかをチェックします。
+    std::cout << expr1 << std::endl;
+    std::cout << expr2 << std::endl;
+    std::cout << expr3 << std::endl;
+    std::cout << expr4 << std::endl;
+    std::cout << expr5 << std::endl;
+    std::cout << expr6 << std::endl;
+    return 0;
+}
